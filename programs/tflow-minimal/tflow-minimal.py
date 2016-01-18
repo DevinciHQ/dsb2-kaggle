36593496cad465c4ecf4e824101472f9b32af7fd
Morten Hustveit <morten.hustveit@gmail.com>
2016-01-18 15:25:59 -0500

    tflow-minimal.py: Perform 10-fold cross-validation

--- programs/tflow-minimal/tflow-minimal.py
+++ programs/tflow-minimal/tflow-minimal.py
@@ -18,6 +18,10 @@ OUTPUT_COUNT = 2
 
 LEARNING_RATE = 0.001
 
+BATCH_COUNT = 200
+
+FOLD_COUNT = 10
+
 # Reads the contents of data/train.csv into a dictionary.
 def ReadTrainingLabels():
   reader = csv.reader(open('data/train.csv', 'r'))
@@ -47,9 +51,9 @@ def LoadTrainingInstance(row):
 
   pixels = np.fromstring(row[4], dtype=np.uint16, count=width * height)
 
+  # Standardize the image data (set mean = 0, and stddev = 1).
   stddev = np.std(pixels)
   assert stddev > 0, stddev
-
   pixels = (pixels - np.mean(pixels)) / stddev
 
   # Reinterpret 1D array as 2D image.
@@ -87,43 +91,64 @@ permutation = np.random.permutation(labels.shape[0])
 dataset = dataset[permutation, :, :]
 labels = labels[permutation, :]
 
-validation_dataset = dataset[:100, :, :]
-validation_labels = labels[:100, :]
-dataset = dataset[100:, :, :]
-labels = labels[100:, :]
+sample_count = len(training_labels)
 
-# Build the graph
-graph = tf.Graph()
-with graph.as_default():
-  tf_train_images = tf.constant(dataset)
-  tf_train_labels = tf.constant(labels)
+# Per-fold validation losses.
+validation_losses = []
 
-  # Fully connected layers need 2D input.
-  tf_flat_images = tf.reshape(tf_train_images, [-1, IMAGE_SIZE * IMAGE_SIZE])
+for fold in range(0, FOLD_COUNT):
+  range_begin = int(fold * sample_count / FOLD_COUNT)
+  range_end = int((fold + 1) * sample_count / FOLD_COUNT)
 
-  # Add a fully connected layer
-  l0_weights = tf.Variable(
-      tf.truncated_normal([IMAGE_SIZE * IMAGE_SIZE, OUTPUT_COUNT]))
-  l0_biases = tf.Variable(tf.zeros([OUTPUT_COUNT]))
-  l0_output = tf.matmul(tf_flat_images, l0_weights) + l0_biases
+  validation_dataset = dataset[range_begin:range_end, :, :]
+  validation_labels = labels[range_begin:range_end, :]
+  if range_begin > 0:
+    train_dataset = np.concatenate([dataset[:range_begin, :, :], dataset[range_end:, :, :]])
+    train_labels = np.concatenate([labels[:range_begin, :], labels[range_end:, :]])
+  else:
+    train_dataset = dataset[range_end:, :, :]
+    train_labels = labels[range_end:, :]
 
-  # L2 loss
-  training_loss = tf.reduce_sum(tf.pow(l0_output - tf_train_labels, 2)) / (2 * len(training_labels))
+  # Build the graph
+  graph = tf.Graph()
+  with graph.as_default():
+    tf.set_random_seed(1234)
 
-  optimizer = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(training_loss)
+    tf_train_images = tf.constant(train_dataset)
+    tf_train_labels = tf.constant(train_labels)
 
-  # The same operations repeated for validation purposes.
-  tf_validation_images = tf.constant(validation_dataset)
-  tf_validation_labels = tf.constant(validation_labels)
-  tf_flat_validation_images = tf.reshape(tf_validation_images, [-1, IMAGE_SIZE * IMAGE_SIZE])
-  l0_validation_output = tf.matmul(tf_flat_validation_images, l0_weights) + l0_biases
-  validation_loss = tf.reduce_sum(tf.pow(l0_validation_output - tf_validation_labels, 2)) / (2 * len(validation_labels))
+    # Fully connected layers need 2D input.
+    tf_flat_images = tf.reshape(tf_train_images, [-1, IMAGE_SIZE * IMAGE_SIZE])
 
-# Train
-with tf.Session(graph=graph) as session:
-  tf.initialize_all_variables().run()
+    # Add a fully connected layer
+    l0_weights = tf.Variable(
+        tf.truncated_normal([IMAGE_SIZE * IMAGE_SIZE, OUTPUT_COUNT]))
+    l0_biases = tf.Variable(tf.zeros([OUTPUT_COUNT]))
+    l0_output = tf.matmul(tf_flat_images, l0_weights) + l0_biases
 
-  for i in range(0, 200):
-    optimizer.run()
-  print('Training loss:   %8.2f' % training_loss.eval())
-  print('Validation loss: %8.2f' % validation_loss.eval())
+    # L2 loss
+    training_loss = tf.reduce_sum(tf.pow(l0_output - tf_train_labels, 2)) / (2 * len(training_labels))
+
+    optimizer = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(training_loss)
+
+    # The same operations repeated for validation purposes.
+    tf_validation_images = tf.constant(validation_dataset)
+    tf_validation_labels = tf.constant(validation_labels)
+    tf_flat_validation_images = tf.reshape(tf_validation_images, [-1, IMAGE_SIZE * IMAGE_SIZE])
+    l0_validation_output = tf.matmul(tf_flat_validation_images, l0_weights) + l0_biases
+    validation_loss = tf.reduce_sum(tf.pow(l0_validation_output - tf_validation_labels, 2)) / (2 * len(validation_labels))
+
+  # Train and evaluate
+  with tf.Session(graph=graph) as session:
+    tf.initialize_all_variables().run()
+
+    for i in range(0, BATCH_COUNT):
+      optimizer.run()
+
+    validation_losses.append(validation_loss.eval())
+
+    print('Fold %s (size=%s):' % (fold, range_end - range_begin))
+    print('  Training loss:   %8.2f' % training_loss.eval())
+    print('  Validation loss: %8.2f' % validation_losses[-1])
+
+print('Aggregate validation loss: mean=%.2f stddev=%.2f' % (np.mean(validation_losses), np.std(validation_losses)))
